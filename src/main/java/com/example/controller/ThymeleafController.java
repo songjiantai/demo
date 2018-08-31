2422e61810553951b4c9dfc85be139d27dbb8c34
Songjiantai <songjiantai@qq.com>
2018-08-31 16:56:06 +0800

    ewew

--- src/main/java/com/example/controller/ThymeleafController.java
+++ src/main/java/com/example/controller/ThymeleafController.java
@@ -30,7 +30,7 @@ public class ThymeleafController {
     public String test(ModelMap map) {
 		
 		User u = new User();
-		u.setName("superadmin");
+		u.setName("lee");
 		u.setAge(10);
 		u.setPassword("123465");
 		u.setBirthday(new Date());
