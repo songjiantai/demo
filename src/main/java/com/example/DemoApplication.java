c973213795d7cd124bd4bd1978f6dbf0faaae13a
Songjiantai <songjiantai@qq.com>
2018-09-03 21:51:10 +0800

    434

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,7 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tk.mybatis.spring.annotation.MapperScan;
+
 @SpringBootApplication
+@MapperScan(basePackages = "com.example.mapper")
 public class DemoApplication {
 
 	public static void main(String[] args) {
