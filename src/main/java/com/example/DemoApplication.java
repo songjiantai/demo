b206b1dd2577edfefb181bbfb95d85c73dd1380c
Songjiantai <songjiantai@qq.com>
2018-09-05 14:43:54 +0800

    4343

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tk.mybatis.spring.annotation.MapperScan;
@@ -12,6 +13,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @ComponentScan(basePackages= {"com.example", "org.n3r.idworker"})
 //开启定时任务
 @EnableScheduling
+//开启异步调用方式
+@EnableAsync
 public class DemoApplication {
 
 	public static void main(String[] args) {
