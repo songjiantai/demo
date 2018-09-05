db1bf1ad3423dc28688bdbf11e989bb577c3194b
Songjiantai <songjiantai@qq.com>
2018-09-05 14:08:53 +0800

    4324

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,12 +3,15 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.example.mapper")
 @ComponentScan(basePackages= {"com.example", "org.n3r.idworker"})
+//开启定时任务
+@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
